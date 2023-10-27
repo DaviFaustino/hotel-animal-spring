7d72c1e0b4a8a265a04f4e8b584800b4bfdbab72
DaviFaustino <davifaustino05@gmail.com>
2023-10-27 11:07:02 -0300

    feat: criacao do modelo Client

---
+++ src/main/java/com/spring/animal/hotel/spring/models/ClientModel.java
@@ -0,0 +1,25 @@
+package com.spring.animal.hotel.spring.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_client")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_cl;
+    private String name_cl;
+    private String phone_cl;
+}
