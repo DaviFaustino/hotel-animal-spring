80443c6c1eb84a934e78ea7081c43d38a78b263d
DaviFaustino <davifaustino05@gmail.com>
2023-10-31 11:32:38 -0300

    feat: criada a tabela de bookins com flyway

---
+++ src/main/resources/db/migration/V2__create-table-bookings.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_bookings (
+    id_bo uuid PRIMARY KEY NOT NULL UNIQUE,
+    date_check_in_bo timestamp NOT NULL,
+    date_check_out_bo timestamp NOT NULL,
+    cost_bo int NOT NULL,
+    id_client_bo int REFERENCES tb_client NOT NULL
+);
