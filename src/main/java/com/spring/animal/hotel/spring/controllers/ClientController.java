02b22fd83517353ff7589a57f381a71accf42f5c
DaviFaustino <davifaustino05@gmail.com>
2023-10-31 11:30:02 -0300

    feat: criacao dos demais verbos http
    Criacao dos metodos que mapeiam os verbos http get,
    put e delete.

--- src/main/java/com/spring/animal/hotel/spring/controllers/ClientController.java
+++ src/main/java/com/spring/animal/hotel/spring/controllers/ClientController.java
@@ -1,10 +1,17 @@
 package com.spring.animal.hotel.spring.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +33,46 @@ public class ClientController {
         ClientModel clientModel = new ClientModel();
         BeanUtils.copyProperties(clientDto, clientModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(clientModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientModel>> getAllClients() {
+        List<ClientModel> clientList = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(clientList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneClient(@PathVariable(value = "id") int id) {
+        Optional<ClientModel> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(optionalClient.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O registro não foi encontrado.");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateCliente(@PathVariable(value = "id") int id, @RequestBody @Valid ClientDto clientDto) {
+        Optional<ClientModel> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            ClientModel clientModel = optionalClient.get();
+            BeanUtils.copyProperties(clientDto, clientModel);
+            
+            return ResponseEntity.status(HttpStatus.OK).body(repository.save(clientModel));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O registro escolhido não existe.");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteClient(@PathVariable(value = "id") int id) {
+        Optional<ClientModel> optionalClient = repository.findById(id);
+        if (optionalClient.isPresent()) {
+            repository.delete(optionalClient.get());
+
+            return ResponseEntity.status(HttpStatus.OK).body("Deleção bem sucedida.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O registro escolhido não existe.");
     }
 }
