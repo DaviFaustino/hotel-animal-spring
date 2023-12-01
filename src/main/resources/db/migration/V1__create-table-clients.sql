f888dc2533cce094a7a559ae89833390301cb162
DaviFaustino <davifaustino05@gmail.com>
2023-12-01 11:52:33 -0300

    refactor: renomeia tabela tb_client nas migrações
    Renomeia tabela tb_client para tb_clients nas migrações
    do flyway.

--- src/main/resources/db/migration/V1__create-table-clients.sql
+++ src/main/resources/db/migration/V1__create-table-clients.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tb_client (
+CREATE TABLE tb_clients (
     id_cl SERIAL PRIMARY KEY NOT NULL UNIQUE,
     name_cl varchar(50) NOT NULL,
     phone_cl char(15) NOT NULL
--- src/main/resources/db/migration/V2__create-table-bookings.sql
+++ src/main/resources/db/migration/V2__create-table-bookings.sql
@@ -3,5 +3,5 @@ CREATE TABLE tb_bookings (
     date_check_in_bo timestamp NOT NULL,
     date_check_out_bo timestamp NOT NULL,
     cost_bo int NOT NULL,
-    id_client_bo int REFERENCES tb_client NOT NULL
+    id_client_bo int REFERENCES tb_clients NOT NULL
 );
