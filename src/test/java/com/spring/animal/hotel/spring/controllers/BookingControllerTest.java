e9209d0aa389d907f0dfeb49a8859b0b9618558b
DaviFaustino <davifaustino05@gmail.com>
2024-02-07 19:43:27 -0300

    test: adiciona teste ao BookingControllerTest

--- src/test/java/com/spring/animal/hotel/spring/controllers/BookingControllerTest.java
+++ src/test/java/com/spring/animal/hotel/spring/controllers/BookingControllerTest.java
@@ -1,5 +1,65 @@
 package com.spring.animal.hotel.spring.controllers;
 
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.spring.animal.hotel.spring.models.BookingModel;
+import com.spring.animal.hotel.spring.models.ClientModel;
+import com.spring.animal.hotel.spring.services.BookingService;
+
+@WebMvcTest(BookingController.class)
 public class BookingControllerTest {
-    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookingService bookingService;
+
+    private BookingModel booking;
+    private ClientModel client;
+
+    @BeforeEach
+    void setup() {
+        client = new ClientModel(1, "Davi Calazans", "(66) 77777-7777");
+
+        booking = new BookingModel(UUID.nameUUIDFromBytes(new byte[1]),
+                                    new Timestamp(1705953600),
+                                    new Timestamp(1706953600),
+                                    1000,
+                                    client);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma reserva com sucesso")
+    void testGetOneBookingCase1() throws Exception {
+        when(bookingService.getOneBooking(any(UUID.class))).thenReturn(booking);
+        
+        mockMvc.perform(get("/bookings/{id}", UUID.nameUUIDFromBytes(new byte[1]).toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id_bo", is(booking.getId_bo().toString())))
+                .andExpect(jsonPath("$.date_check_in_bo", is("1970-01-20T17:52:33.600+00:00")))
+                .andExpect(jsonPath("$.date_check_out_bo", is("1970-01-20T18:09:13.600+00:00")))
+                .andExpect(jsonPath("$.cost_bo", is(booking.getCost_bo())));
+
+        verify(bookingService).getOneBooking(any());
+        verifyNoMoreInteractions(bookingService);
+    }
+
 }
