4f12ba4a5fab514c805d47beba954f2c793f5e34
DaviFaustino <davifaustino05@gmail.com>
2024-02-08 11:56:57 -0300

    feat: adiciona content-type a resposta de controlador

--- src/main/java/com/spring/animal/hotel/spring/controllers/BookingController.java
+++ src/main/java/com/spring/animal/hotel/spring/controllers/BookingController.java
@@ -1,10 +1,14 @@
 package com.spring.animal.hotel.spring.controllers;
 
+import java.net.http.HttpHeaders;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,7 +56,9 @@ public class BookingController {
             return ResponseEntity.status(HttpStatus.OK).body(serviceResponse);
         }
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(serviceResponse);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                            .header("Content-Type", "application/json;charset=UTF-8")
+                            .body(serviceResponse);
     }
 
     @PutMapping("/{id}")
