2ecb68f60ac642be4ece880e28ccb59ba849ab56
DaviFaustino <davifaustino05@gmail.com>
2024-02-15 19:05:17 -0300

    feat: adiciona doc swagger ao controller de reservas

--- src/main/java/com/spring/animal/hotel/spring/controllers/BookingController.java
+++ src/main/java/com/spring/animal/hotel/spring/controllers/BookingController.java
@@ -24,6 +24,9 @@ import com.spring.animal.hotel.spring.models.BookingDto;
 import com.spring.animal.hotel.spring.models.BookingModel;
 import com.spring.animal.hotel.spring.services.BookingService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -32,6 +35,11 @@ public class BookingController {
     @Autowired
     BookingService bookingService;
 
+    @Operation(summary = "Salva uma nova reserva")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Reserva salva com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Id do cliente não encontrado")
+    })
     @PostMapping
     public ResponseEntity<Object> saveBooking(@RequestBody @Valid BookingDto bookingDto) {
         Object serviceResponse = bookingService.saveBooking(bookingDto);
@@ -43,11 +51,20 @@ public class BookingController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(serviceResponse);
     }
     
+    @Operation(summary = "Retorna todas as reservas")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Todas as reservas retornadas com sucesso")
+    })
     @GetMapping
     public ResponseEntity<List<BookingModel>> getAllBookings() {
         return ResponseEntity.status(HttpStatus.OK).body(bookingService.getAllBookings());
     }
 
+    @Operation(summary = "Retorna uma reserva pelo id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Reserva retornada com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Reserva não encontrada")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Object> getOneBooking(@PathVariable(value = "id") UUID id) {
         Object serviceResponse = bookingService.getOneBooking(id);
@@ -61,6 +78,11 @@ public class BookingController {
                             .body(serviceResponse);
     }
 
+    @Operation(summary = "Atualiza reserva pelo id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Reserva atualizada com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Reserva não encontrada")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateBooking(@PathVariable(value = "id") UUID id, @RequestBody @Valid BookingDto bookingDto) {
         Object serviceResponse = bookingService.updateBooking(bookingDto, id);
@@ -72,6 +94,11 @@ public class BookingController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(serviceResponse);
     }
 
+    @Operation(summary = "Aplica desconto na reserva pelo id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Desconto aplicado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Reserva não encontrada")
+    })
     @PatchMapping("/apply-discount/{id}")
     public ResponseEntity<Object> applyDiscountBooking(@PathVariable(value = "id") UUID id) {
         Object serviceResponse = bookingService.applyDiscountBooking(id);
@@ -85,6 +112,11 @@ public class BookingController {
                             .body(serviceResponse);
     }
 
+    @Operation(summary = "Apagar uma reserva pelo id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Reserva apagada com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Reserva não encontrada")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteBooking(@PathVariable(value = "id") UUID id) {
         if (bookingService.deleteBooking(id)) {
