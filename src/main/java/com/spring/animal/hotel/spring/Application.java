3ae260fd79e639dbb996ea19532abd6038db3fe2
DaviFaustino <davifaustino05@gmail.com>
2024-02-15 18:57:06 -0300

    feat: adiciona dependencia do swagger e configura

--- src/main/java/com/spring/animal/hotel/spring/Application.java
+++ src/main/java/com/spring/animal/hotel/spring/Application.java
@@ -3,7 +3,11 @@ package com.spring.animal.hotel.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Hotel API", version = "1", description = "API do Hotel Animal para ser consumida pelo front-end."))
 public class Application {
 
 	public static void main(String[] args) {
